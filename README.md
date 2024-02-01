a833dc67be784c47a036d08cfc8d0b3de19775b3
Stian Sandberg <stian@stian.net>
2024-02-01 13:27:59 +0100

    Chess ELO

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# ChessELO
+# Sandberg.ChessELO
+
+Easy to use library to calculate Chess ELO rating and ELO performance
+
+### Calculate ELO rating
+```c#
+RatingResult Sandberg.Chess.ELO.CalculateNewRating(double, double, GameResult, int?, int?);
+```
+
+```c#
+# Example
+
+double playerWhite = 1298; // Set white player rating
+double playerBlack = 1485; // Set black player rating
+var result = Sandberg.Chess.ELO.GameResult.White; // White/Remis/Black
+var ratingResult = Sandberg.Chess.ELO.CalculateNewRating(playerWhite, playerBlack, result);
+```
+
+You can set kFactor for both white player and black player. Default is 20 for both.
+
+CalculateNewRating will return a `Sandberg.Chess.ELO.RatingResult` object:
+```js
+{
+  "Winner": 1,
+  "White": {
+    "KFactor": 20,
+    "ExpectedScore": 0.6400649998028851,
+    "Score": 1,
+    "OldRating": 1300,
+    "NewRating": 1307.1987000039423,
+    "RatingAdjustment": 7.198700003942349
+  },
+  "Black": {
+    "KFactor": 20,
+    "ExpectedScore": 0.35993500019711494,
+    "Score": 0,
+    "OldRating": 1200,
+    "NewRating": 1192.8012999960577,
+    "RatingAdjustment": -7.198700003942349
+  }
+}
+```
+
+### Calculate performance rating
+Performance rating is a hypothetical rating that would result from the games of a single event only.
+According to this algorithm, performance rating for an event is calculated in the following way:
+- For each win, add your opponent's rating plus 400 *(factor)*,
+- For each remis, add your opponent's rating
+- For each loss, add your opponent's rating minus 400 *(factor)*,
+- And divide this sum by the number of played games.
+
+(You can use a different *factor* you like, but 400 is default)
+
+```c#
+double Sandberg.Chess.ELO.RatingPerformance(double[], double[], double[], int?);
+```
+
+```c#
+double[] wins = [1200, 1300, 1100, 1600];
+double[] remis = [1245, 1300];
+double[] losses = [1500];
+
+var performance = Sandberg.Chess.ELO.RatingPerformance(wins, remis, losses);
+// performance rating --> 1492,142857142857
+```
---
+++ src/Sandberg.Chess.ELO/ChessELO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace Sandberg.Chess
+{
+    public class ELO
+    {
+        public enum GameResult { White = 1, Remis = 2, Black = 3 }
+
+        public static RatingResult CalculateNewRating(double ratingPlayerWhite, double ratingPlayerBlack, GameResult result, int kFaktorWhite = 20, int kFaktorBlack = 20)
+        {
+            double _scoreWhite = result == GameResult.White ? 1 : result == GameResult.Remis ? 0.5 : 0;
+            double _scoreBlack = result == GameResult.Black ? 1 : result == GameResult.Remis ? 0.5 : 0;
+
+            var expectedScores = GetExpectedScore(ratingPlayerWhite, ratingPlayerBlack);
+            var newRatings = GetNewRatings(ratingPlayerWhite, ratingPlayerBlack, expectedScores.playerWhite, expectedScores.playerBlack, _scoreWhite, _scoreBlack, kFaktorWhite, kFaktorBlack);
+
+            return new RatingResult
+            {
+                Winner = result,
+                White = new RatingPlayerResult(ratingPlayerWhite, newRatings.newRatingWhite, expectedScores.playerWhite, _scoreWhite, kFaktorWhite),
+                Black = new RatingPlayerResult(ratingPlayerBlack, newRatings.newRatingBlack, expectedScores.playerBlack, _scoreBlack, kFaktorBlack)
+            };
+        }
+
+        public static double RatingPerformance(double[] wins, double[] remis, double[] losses, int factor = 400)
+        {
+            var w = wins.Sum() + (wins.Count() * factor);
+            var r = remis.Sum();
+            var l = losses.Sum() - (losses.Count() * factor);
+            var n = wins.Count() + remis.Count() + losses.Count();
+            return (w + r + l) / n;
+        }
+
+        public static (double playerWhite, double playerBlack) GetExpectedScore(double ratingWhite, double ratingBlack)
+        {
+            double expectedScoreWhite = 1 / (1 + (Math.Pow(10, (ratingBlack - ratingWhite) / 400)));
+            double expectedScoreBlack = 1 / (1 + (Math.Pow(10, (ratingWhite - ratingBlack) / 400)));
+            return (expectedScoreWhite, expectedScoreBlack);
+        }
+
+        private static (double newRatingWhite, double newRatingBlack) GetNewRatings(double ratingWhite, double ratingBlack, double expectedWhite, double expectedBlack, double scoreWhite, double scoreBlack, int kFaktorWhite, int kFaktorBlack)
+        {
+            double newRatingA = ratingWhite + (kFaktorWhite * (scoreWhite - expectedWhite));
+            double newRatingB = ratingBlack + (kFaktorBlack * (scoreBlack - expectedBlack));
+            return (newRatingA, newRatingB);
+        }
+
+
+        public class RatingResult
+        {
+            public GameResult Winner { get; internal set; } = GameResult.Remis;
+            public RatingPlayerResult White { get; internal set; } = new RatingPlayerResult();
+            public RatingPlayerResult Black { get; internal set; } = new RatingPlayerResult();
+        }
+
+        public class RatingPlayerResult
+        {
+            public RatingPlayerResult() { }
+            public RatingPlayerResult(double oldRating, double newRating, double expectedScore, double score, int kFactor) { NewRating = newRating; OldRating = oldRating; ExpectedScore = expectedScore; Score = score; KFactor = kFactor; }
+
+            public int KFactor { get; private set; }
+            public double ExpectedScore { get; private set; }
+            public double Score { get; private set; }
+            public double OldRating { get; private set; }
+            public double NewRating { get; private set; }
+            public double RatingAdjustment => NewRating - OldRating;
+        }
+    }
+}
